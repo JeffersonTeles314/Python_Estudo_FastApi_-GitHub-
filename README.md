1ab5c75caa3996b1420b2e1403c30dd60d773b1d
Jefferson Teles <58746739+JeffersonTeles314@users.noreply.github.com>
2023-10-22 12:55:50 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Python_Estudo_FastApi_-GitHub-
+ 
---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+vendas = { 1: {"item": "lata", "preco_unitario": 4, "quantidade": 5},
+           2:{"item": "garrafa 2L", "preco_unitario": 15, "quantidade":5},
+           3: {"item": "garrafa 750ml", "preco_unitario": 10, "quantidade":5},
+           4: {"item": "lata mini", "preco_unitario": 2, "quantidade":5}, }
+
+@app.get('/')
+def main():
+    return "Main Page"
+
+@app.get('/tailskko')
+def tailskko():
+    return "tailskko"
+
+@app.get("/users/{name}")
+def get_user(name: str):
+    return {"user": {"name": name}}
